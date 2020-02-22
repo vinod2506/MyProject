7723465742587c6ed31722a3ba800d9f72a03be1
vinodwus <vinodwus@DESKTOP-VCDI392>
2020-02-22 13:09:07 +0530

    resave

--- MyProject/src/main/java/in/nit/Employee.java
+++ MyProject/src/main/java/in/nit/Employee.java
@@ -1,10 +1,17 @@
 package in.nit;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
-	
+	@Id
 	private int eid;
+	@Column
 	private String ename;
+	@Column
 	private String addrs;
 	
 }
